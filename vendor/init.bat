18df3859b9c5fa74d4f7b80463405ca272de40a1
Morten Lindhardt <r3fuze@gmail.com>
2015-01-26 22:53:47 +0100

    Small updates to init.bat

--- vendor/init.bat
+++ vendor/init.bat
@@ -8,8 +8,9 @@
 )
 
 :: Change the prompt style
-:: Mmm tasty lamb
-@prompt $E[1;32;40m$P$S{git}$S$_$E[1;30;40m{lamb}$S$E[0m
+:: Mmm tasty lamb NOPE!
+:: Prompt is now handled in 'prompt.lua'
+:: @prompt $E[1;32;40m$P$S{git}$S$_$E[1;30;40m{lamb}$S$E[0m
 
 :: Pick right version of clink
 @if "%PROCESSOR_ARCHITECTURE%"=="x86" (
@@ -30,17 +31,19 @@
 :: Enhance Path
 @set git_install_root=%CMDER_ROOT%\vendor\msysgit
 
+:: Should we check if the file exists? @if exist %include_path%
 @set include_path=%CMDER_ROOT%\bin\include
 
 :: Using 'for /f' to assign output of a command to a variable.
 :: 'type' command is used to output contents of 'include file'
-:: 'tr' command is used to replace newlines with semi colons. Using %git_install_root% in case user doesn't have git installed.
+:: 'tr' command is used to replace newlines with semi colons.
+:: Included git commands are not loaded at this points so we are using %git_install_root% just in case the user doesn't have git installed.
 @for /f "delims=" %%i in ('type %include_path% ^| %git_install_root%\bin\tr "\\n" ";"') do @set include_content=%%i
 
 :: Using 'call' to expand environment variables inside string.
 @call set include_content=%include_content%
 
-@set PATH=%CMDER_ROOT%\bin;%git_install_root%\bin;%git_install_root%\mingw\bin;%git_install_root%\cmd;%git_install_root%\share\vim\vim74;%include_content%;%CMDER_ROOT%;%PATH%
+@set PATH=%CMDER_ROOT%\bin;%git_install_root%\bin;%git_install_root%\cmd;%git_install_root%\share\vim\vim74;%include_content%;%CMDER_ROOT%;%PATH%
 
 :: Add aliases
 @doskey /macrofile="%CMDER_ROOT%\config\aliases"
